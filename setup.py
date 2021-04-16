7f456df654f9e9dedff428cc96389ab2fa67b4e5
Nick Korotysh <kolchaprogrammer@list.ru>
2021-04-17 00:43:33 +0300

    added more metadata

--- setup.py
+++ setup.py
@@ -8,7 +8,14 @@ setuptools.setup(
     version="0.1.2",
     author="Nick Korotysh",
     author_email="kolchaprogrammer@list.ru",
+    description="download torrents from shiza-project.com",
+    url="https://github.com/Kolcha/shiza-utils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=setuptools.find_packages(),
     install_requires=['lxml', 'requests'],
-    python_requires='>=3',
+    python_requires='>=3.6',
 )
