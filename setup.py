8ae314cb3dc578f4c9d707e849b146bf5b4621b6
Nick Korotysh <kolchaprogrammer@list.ru>
2021-05-09 15:40:21 +0300

    version 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="shiza-utils",
-    version="0.2.0",
+    version="0.2.1",
     author="Nick Korotysh",
     author_email="kolchaprogrammer@list.ru",
     description="download torrents from shiza-project.com",
